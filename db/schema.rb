7fec83b3c298969153d7909e737300d636785a84
Stephen Grieves <stephengrieves@gmail.com>
2022-02-28 17:57:39 +0100

    set-up and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_134530) do
+ActiveRecord::Schema.define(version: 2022_02_28_164615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.text "details"
+    t.boolean "mark_as_done"
+    t.integer "priority"
+    t.datetime "start_at"
+    t.integer "duration"
+    t.integer "days", array: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +41,5 @@ ActiveRecord::Schema.define(version: 2022_02_28_134530) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
