62e264d8ae39fa2eaf75511a6d645d8aa19a4e15
Stephen Grieves <stephengrieves@gmail.com>
2022-03-02 16:32:29 +0100

    new and create form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,29 @@ class TasksController < ApplicationController
 
   def index
     @tasks = policy_scope(Task)
-
   end
+
+  def new
+    @task = Task.new
+    authorize @task
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.user = current_user
+    authorize @task
+    raise
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :details, :priority, :start_at, :duration, days:[])
+  end
+
 end
