d9c1746d1c0ff7284d62ea254ec7388712fae44c
Matthieu Villeret <matthieuvilleret@yahoo.fr>
2022-03-02 15:31:21 +0100

    check box

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,6 @@ class Task < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
-  validates :mark_as_done, default: false
-  validates :priority, default: 3
+  # validates :mark_as_done, default: false
+  # validates :priority, default: 3
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :tasks, only: [:index]
+  resources :tasks, only: [:index] do
+    member do
+      patch :mark_as_done
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
