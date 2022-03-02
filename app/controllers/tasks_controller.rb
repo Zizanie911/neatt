38f293eac59b8eb386f93c0821e6b81cbc027a84
Lilia Bedidi <lilia.bedidi@gmail.com>
2022-03-02 12:33:07 +0100

    create 3 count methods for habits tasks and total

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,5 +4,26 @@ class TasksController < ApplicationController
     @tasks = policy_scope(Task)
     @user = current_user
     @username = @user.username
+    @total = nb_total_tasks
+    @nb_habits = nb_habits
+    @nb_tasks = nb_tasks
+  end
+
+  private
+
+  def nb_total_tasks
+    @tasks.count
+  end
+
+  def nb_habits
+    nb = 0
+    @tasks.each do |task|
+      nb += 1 if task.days.nil?
+    end
+    return nb
+  end
+
+  def nb_tasks
+    return nb_total_tasks - nb_habits
   end
 end
